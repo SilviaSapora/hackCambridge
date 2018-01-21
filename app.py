0516bf759a5673ebfceaa38981bdf4c8d2e8f1d5
Silvia Sapora <silvia.sapora@gmail.com>
2018-01-21 10:04:13 +0000

    add new obj

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import os
 from flask import Flask, request
 from flask_restful import Resource, Api
+import json
 from json import dumps
 from flask import send_file
 
@@ -23,7 +25,38 @@ class Picture(Resource):
     def get(self, obj_id):
         filename = "image" + obj_id + ".jpg"
         return send_file(filename, mimetype='image')
- 
+
+class Upload(Resource):
+    def get(self, obj_id, obj_prediction, obj_date, obj_exp):
+        with open("obj.txt", "rb") as obj_file:
+            f = obj_file.read()
+            jsobj = json.loads(f)
+        app_data = {}
+        app_data['obj_exp'] = obj_exp
+        app_data['obj_prediction'] = obj_prediction
+        app_data['obj_date'] = obj_date
+        app_data['obj_id'] = obj_id
+        json_data = json.dumps(app_data)
+        jsobj["objects"].append(app_data)
+        file_updated_data = json.dumps(jsobj)
+        os.remove("obj.txt")
+        with open('obj.txt', 'a') as the_file:
+            the_file.write(file_updated_data)
+        return "Uploaded successfully!"
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload/<string:filename>', methods=['GET', 'POST'])
+def upload_file(filename):
+    if request.method == 'POST':
+            file.save(os.path.join(filename))
+            return "File uploaded successfully"
+
+api.add_resource(Upload, '/new_object/<string:obj_id>/<string:obj_prediction>/<string:obj_date>/<string:obj_exp>')
 api.add_resource(Objects, '/obj')
 api.add_resource(Picture, '/picture/<string:obj_id>')
 
