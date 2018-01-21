01a5570898680bc25bd93f7114743377f1dfa3b6
Silvia Sapora <silvia.sapora@gmail.com>
2018-01-21 07:22:00 +0000

    rest-api and rest-app added

---
+++ rest-app/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask import send_file
+
+
+app = Flask(__name__)
+api = Api(app)
+
+class Objects(Resource):
+    def get(self):
+        with open('../objects.txt', 'r') as myfile:
+            data = myfile.read()
+        print(data)
+
+        return data
+
+class Picture(Resource):
+    def get(self, obj_id):
+        filename = "image" + obj_id + ".jpg"
+        return send_file(filename, mimetype='image')
+ 
+api.add_resource(Objects, '/obj')
+api.add_resource(Picture, '/picture/<string:obj_id>')
+
+if __name__ == '__main__':
+     app.run()
