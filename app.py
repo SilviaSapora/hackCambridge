51ce6eab0d9d4670bf0fcf009e7b6c375939b024
Silvia Sapora <silvia.sapora@gmail.com>
2018-01-28 00:07:00 +0000

    now prediction is function

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_restful import Resource, Api
 import json
 from json import dumps
 from flask import send_file
-
+import prediction
 
 app = Flask(__name__)
 api = Api(app)
@@ -22,31 +22,12 @@ class Objects(Resource):
         return data
 
 class Picture(Resource):
-    def get(self, obj_id):
-        filename = obj_id + ".jpg"
+    def get(self, image):
+        print(image)
+        api(image)        
         return send_file(filename, mimetype='image')
 
-@app.route('/new_object/<string:obj_id>/<string:obj_prediction>/<string:obj_date>/<string:obj_exp>')
-def new_object(obj_id, obj_prediction, obj_date, obj_exp):
-    with open("obj.txt", "rb") as obj_file:
-        f = obj_file.read()
-        jsobj = json.loads(f)
-    app_data = {}
-    app_data['obj_exp'] = obj_exp
-    app_data['obj_prediction'] = obj_prediction
-    app_data['obj_date'] = obj_date
-    app_data['obj_id'] = obj_id
-    json_data = json.dumps(app_data)
-    jsobj["objects"].append(app_data)
-    file_updated_data = json.dumps(jsobj)
-    os.remove("obj.txt")
-    with open('obj.txt', 'a') as the_file:
-        the_file.write(file_updated_data)
-    return "Uploaded successfully!"
-
-#api.add_resource(Upload, '/new_object/<string:obj_id>/<string:obj_prediction>/<string:obj_date>/<string:obj_exp>')
-api.add_resource(Objects, '/obj')
-api.add_resource(Picture, '/picture/<string:obj_id>')
+api.add_resource(Picture, '/picture/<string:image>')
 
 if __name__ == '__main__':
      app.run()
--- prediction.py
+++ prediction.py
@@ -5,47 +5,49 @@ import webbrowser
 
 
 #################### PREDICTION ####################
-
-pre_headers = {
+function api(image):
+    pre_headers = {
         # Request headers
         'Prediction-key': '7a3c2853276a42abb5bb9c709433ec3d',
-        }
+    }
 
-pre_params = urllib.urlencode({
+    pre_params = urllib.urlencode({
         # Request parameters
         'projectId': '34a4628f-9ab4-4732-8db0-9116ff433aa5'
         })
 
-with open("keypic.jpg", "rb") as imageFile:
-  f = imageFile.read()
-  body = bytearray(f)
+    #with open("keypic.jpg", "rb") as imageFile:
+        #f = imageFile.read()
+        #body = bytearray(f)
 
-#body = "{'url':'https://i2.wp.com/www.peanutbutterlist.com/wp-content/uploads/2015/02/groceries.jpg?fit=1000%2C508'}"
+        #body = "{'url':'https://i2.wp.com/www.peanutbutterlist.com/wp-content/uploads/2015/02/groceries.jpg?fit=1000%2C508'}"
 
-try:
-    conn = httplib.HTTPSConnection('southcentralus.api.cognitive.microsoft.com')
-    conn.request("POST", "/customvision/v1.1/Prediction/be7c629e-3887-4622-92b9-02df9f0315c6/image?%s" % pre_params, body, pre_headers)
-    response = conn.getresponse()
-    pre_data = response.read()
-    print("PRE-DATA")
-    print(pre_data)
-    conn.close()
+    try:
+        conn = httplib.HTTPSConnection('southcentralus.api.cognitive.microsoft.com')
+        conn.request("POST", "/customvision/v1.1/Prediction/be7c629e-3887-4622-92b9-02df9f0315c6/image?%s" % pre_params, body, pre_headers)
+        response = conn.getresponse()
+        pre_data = response.read()
+        print("PRE-DATA")
+        print(pre_data)
+        conn.close()
 
-except Exception as e:
+    except Exception as e:
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
-pre_json_data = json.loads(pre_data)
-obj_prediction = pre_json_data["Predictions"][0]["Tag"]
-obj_id = pre_json_data["Id"]
-obj_date = pre_json_data["Created"]
+        pre_json_data = json.loads(pre_data)
+        obj_prediction = pre_json_data["Predictions"][0]["Tag"]
+        obj_id = pre_json_data["Id"]
+        obj_date = pre_json_data["Created"]
 
-app_data = {}
-app_data['obj_prediction'] = obj_prediction
-app_data['obj_date'] = obj_date
-app_data['obj_id'] = obj_id
-json_data = json.dumps(app_data)
+        app_data = {}
+        app_data['obj_prediction'] = obj_prediction
+        app_data['obj_date'] = obj_date
+        app_data['obj_id'] = obj_id
+        json_data = json.dumps(app_data)
 
-#webbrowser.open('https://fierce-gorge-21914.herokuapp.com/new_object/'+ obj_id + "/" + obj_prediction + "/" + obj_date + "/0")
+        #webbrowser.open('https://fierce-gorge-21914.herokuapp.com/new_object/'+ obj_id + "/" + obj_prediction + "/" + obj_date + "/0")
 
-print("JSON-DATA")
-print(json_data)
+        print("JSON-DATA")
+        print(json_data)
+        return json_data
+
